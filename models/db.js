9d0f6d8a98060ba45068eeb1d8489a780ea1b200
Roid <132744222+TWCS911@users.noreply.github.com>
2025-01-12 19:15:44 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-    // online
-    "mongodb+srv://admin:adminapotek123@apotek.bbncw.mongodb.net/?retryWrites=true&w=majority&appName=apotek"
-  ).then(()=>{
+mongoose.connect("mongodb+srv://admin:adminapotek123@apotek.bbncw.mongodb.net/?retryWrites=true&w=majority&appName=apotek")
+  .then(() => {
     console.log("Connected to database");
-  }).catch(()=>{
-    // console.error("App Starting error: ", err.stack);
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
     console.log("Connected failed");
   });
